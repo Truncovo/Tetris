2fb4e484916a7839730f70325760814f668d60fc
Jakub Trunecek <trunecek.jakub@gmail.com>
2018-08-25 21:43:46 +0000

    AzureDatabaseTesting

--- TetrisEngine/MainClass.cs
+++ TetrisEngine/MainClass.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using WindowsAndInput;
+using TetrisEngine.AzureDatabase;
 
 namespace TetrisEngine
 {
@@ -8,10 +9,19 @@ namespace TetrisEngine
         [STAThread]
         public static void Main()
         {
+            //var context = new TruncovoEntities1();
+            //var highScore = new HighScore
+            //{
+            //    LogID = 1,
+            //    PlayerName = "Trunec",
+            //    Score = 25,
+            //    timeStamp = DateTime.Now
+            //};
+            //context.HighScores.Add(highScore);
+            //context.SaveChanges();
+
             GeneralWindow.Get.WindowSetting = new PickLvl();
             GeneralWindow.Get.Run();
         }
     }
-
-
 }
